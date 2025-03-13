16e5c5955bd900dd659ff27357bbbda481a75a99
yashr <yashraj97710@gmail.com>
2025-03-13 14:53:24 +0530

    Adding local repo to github

---
+++ Calculator.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# OkOk
+#Autor : Yash Raj
+#
+#Addition Function
+#
+x = a + b
+
+#Subtraction Function
+#
+x = a - b
+
+#Division Function
+#
+x = a / b
+
+# Multiplication
+#
+x = a * b
+
+#Percentage
+#
+#
+#
+#heheh
+#
+#
+#
+#tested
